ff2964bb56ceafd0b520a9a8bc13933e616403f2
Claytongirl <lisadeclayton@gmail.com>
2020-11-23 13:41:02 -0700

    initial commit InfoQuest Nov

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>InfoQuest App
+        </h1>
+        <h2>Lisa</h2>
+      </div>
+    );
+  }
+}
